809a25916333afff9e5f525ba60c16f7b490accd
Natp24109 <nathanp2402@gmail.com>
2024-12-30 23:07:22 +0100

    Added swapping pokémon option in battle

--- duel.c
+++ duel.c
@@ -8,6 +8,20 @@ typedef struct{
 
 float statVariations[13]={0.25,2./7,1./3,2./5,0.5,2./3,1,1.5,2,2.5,3,3.5,4};
 
+void printTeam(t_Team * t){
+	/*
+	To do:
+	ADD condition if the team has less than 6 poke
+	*/
+	for(int i=0;i<6;i++){
+		if (1){ //Insert condition here
+			printPoke(&(t->team[i]));
+			getchar();
+			system("clear");
+		}
+	}
+}
+
 int calcStatFrom(t_Poke * p,int stat,int includeVariations){
 	if (stat==PV) return ((int)(2*p->baseStats[PV]+p->iv[PV])*p->lvl/100)+p->lvl+10;
 	int value=(int)(((2*p->baseStats[stat]+p->iv[stat])*p->lvl/100)+5)*tabNature[p->nature].coeff[stat];
@@ -51,12 +65,20 @@ int calcDamage(t_Poke * offender,t_Poke * defender,int indexMove){
 	*/
 }
 
+void setDefaultStatChanges(t_Poke * p){
+	for(int i=0;i<6;i++) p->statChanges[i]=NEUTRAL_STAT_CHANGE;
+}
+
 int isAlive(t_Poke * p){
 	return p->current_pv!=0;
 }
 
 int isAttacking(int move){
-	return move<4;
+	return move<4 && move>=0;
+}
+
+int isSwitching(int move){
+	return move>=11 && move<16;
 }
 
 int PriorityForFirstPoke(t_Poke * p1, t_Poke * p2,int move1,int move2){
@@ -76,6 +98,14 @@ void affectDamage(t_Poke * offender, t_Poke * defender, int indexMove){
 	defender->current_pv=defender->current_pv>damage?defender->current_pv - damage:0;
 }
 
+void swapActualAttacker(t_Team * t, int swapIndex){
+	t_Poke sauv=t->team[0];
+	t->team[0]=t->team[swapIndex-10];
+	t->team[swapIndex-10]=sauv;
+	setDefaultStatChanges(&(t->team[0]));
+	printf("Swapping Happened\n\n");
+}
+
 void playATurn(t_Team * t1, int move1, t_Team * t2, int move2){
 	/*
 		use speed of pokemon to determine order
@@ -87,28 +117,40 @@ void playATurn(t_Team * t1, int move1, t_Team * t2, int move2){
 		if(isAttacking(move1)){
 			affectDamage(&(t1->team[0]),&(t2->team[0]),move1);
 		}
+		else if (isSwitching(move1)){
+			swapActualAttacker(t1,move1);
+		}
 		else{
 			return;
 		}
 		if(isAlive(&(t2->team[0]))){
-			if(isAttacking(move2)){
+			if (isAttacking(move2)){
 				affectDamage(&(t2->team[0]),&(t1->team[0]),move2);
 			}
+			else if (isSwitching(move2)){
+				swapActualAttacker(t2,move2);
+			}	
 			else{
 				return;
 			}
 		}
 	}
 	else{
-		if(isAttacking(move2)){
+		if (isAttacking(move2)){
 			affectDamage(&(t2->team[0]),&(t1->team[0]),move2);
+		}
+		else if (isSwitching(move2)){
+			swapActualAttacker(t2,move2);
 		}
 		else{
 			return;
 		}
-		if(isAlive(&(t1->team[0]))){
+		if (isAlive(&(t1->team[0]))){
 			if(isAttacking(move1)){
 				affectDamage(&(t1->team[0]),&(t2->team[0]),move1);
+			}
+			else if (isSwitching(move1)){
+				swapActualAttacker(t1,move1);
 			}
 			else{
 				return;
@@ -118,6 +160,23 @@ void playATurn(t_Team * t1, int move1, t_Team * t2, int move2){
 
 }
 
+void testBattle(t_Team * rouge, t_Team * bleu){
+	printf("pv rouge : %d\n\n",rouge->team[0].current_pv);
+	printf("pv bleu : %d\n\n",bleu->team[0].current_pv);
+	while(isAlive(&(rouge->team[0])) && isAlive(&(bleu->team[0]))){
+		playATurn(rouge,rand()%4,bleu,rand()%4);
+		printf("pv rouge : %d\n\n",rouge->team[0].current_pv);
+		printf("pv bleu : %d\n\n",bleu->team[0].current_pv);
+	}
+}
+
+void testSwitch(t_Team * rouge, t_Team * bleu){
+	printTeam(rouge);
+	int move1=rand()%5+11;
+	int move2=rand()%4;
+	playATurn(rouge,move1,bleu,move2);
+	printTeam(rouge);
+}
 
 int main(){
 	initData();
@@ -125,14 +184,6 @@ int main(){
 	t_Team bleu;
 	initTeam(&rouge);
 	initTeam(&bleu);
-	printPoke(&rouge.team[0]);
-	printPoke(&bleu.team[0]);
-	printf("pv rouge : %d\n\n",rouge.team[0].current_pv);
-	printf("pv bleu : %d\n\n",bleu.team[0].current_pv);
-	while(isAlive(&(rouge.team[0])) && isAlive(&(bleu.team[0]))){
-		playATurn(&rouge,rand()%4,&bleu,rand()%4);
-		printf("pv rouge : %d\n\n",rouge.team[0].current_pv);
-		printf("pv bleu : %d\n\n",bleu.team[0].current_pv);
-	}
+	testSwitch(&rouge,&bleu); //use testSwitch or testBattle to try the program's features
 	return 0;
 }
