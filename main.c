1285a3e20b908db5d235c7a6d8def6af79cc4bf2
Lounol72 <subtil.louis2005@gmail.com>
2025-01-02 19:02:46 +0100

    Ajout de la gestion audio

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
+#include <SDL2/SDL_mixer.h>
 #include <stdio.h>
 #include "boutons.h"
 #include "inputs.h"
@@ -20,7 +21,13 @@ int main(int argc, char* argv[]) {
     State currentState = MENU;
 
     if (SDL_Init(SDL_INIT_VIDEO) < 0) quitSDL(1,NULL);
-
+    if (SDL_Init(SDL_INIT_AUDIO) < 0) quitSDL(1,NULL);
+    if (Mix_OpenAudio(96000, MIX_DEFAULT_FORMAT, MIX_DEFAULT_CHANNELS, 1024) < 0) // création de la configuration de la carte son
+    {
+        SDL_Log("Erreur initialisation SDL_mixer : %s", Mix_GetError());
+        SDL_Quit();
+        return -1;
+    }
     window = SDL_CreateWindow("ICPocket", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 720, 600, SDL_WINDOW_SHOWN);
 
     SDL_Surface* image = IMG_Load("assets/MENU_TEST.png");
@@ -34,6 +41,10 @@ int main(int argc, char* argv[]) {
     
     mainLoop(window, image, &backgroundColor, &currentState);
 
+    SDL_FreeSurface(menu);
+    SDL_FreeSurface(parametre);
+    SDL_DestroyWindow(window);
+    Mix_CloseAudio();
     quitSDL(0,window);
 
     return 0;
@@ -41,6 +52,23 @@ int main(int argc, char* argv[]) {
 
 void mainLoop(SDL_Window* window, SDL_Surface* image, int* backgroundColor, State* currentState) {
     SDL_Surface* menu = NULL;
+
+    SDL_AudioSpec spec;
+
+    SDL_memset(&spec, 0, sizeof(spec));
+
+    spec.freq = 96000; // 4 100 Hz, 48 000 Hz, 96 000 Hz, 192 000 Hz (standard) 
+    spec.format = AUDIO_F32SYS;
+    spec.channels = 1;
+    spec.samples = 4096; // Oublier pas que ce sa doit être en puissance de deux 2^n
+    spec.callback = [](void* param, Uint8* stream, int len)
+    {
+        // Envoyez les données dans notre buffer...
+    };
+
+    SDL_AudioDeviceID dev = SDL_OpenAudioDevice(nullptr, 0, &spec, &spec, SDL_AUDIO_ALLOW_FREQUENCY_CHANGE);
+
+    SDL_PauseAudioDevice(dev, SDL_FALSE);
 
     while (1) {
         menu = SDL_GetWindowSurface(window);
