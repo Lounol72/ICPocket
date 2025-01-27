20d69f76eebc4a3cb1c634c803fe040b28257a24
Natp24109 <nathanp2402@gmail.com>
2025-01-27 14:43:01 +0100

    fix files

--- src/trainerAI.c
+++ src/trainerAI.c
@@ -1,13 +1,13 @@
 #include "include/structPoke.h"
 #include "include/duel.h"
 #include "include/trainerAI.h"
-/*
+
 t_AI iaTest={10,none,&bleu};
 
-int AI_move_choice(t_Team * ai, t_Team * p){
+int AI_move_choice(t_AI * ai, t_Team * p){
 	int tab_damage[4]={-1};
 	for(int i=0;i<4;i++){
-		tab_damage[i]=calcDamage()
+		tab_damage[i]=calcDamage(ai->AI_t_Team,p,&(ai->AI_t_Team->team[0].moveList[i]));
 	}
+	return 0;
 }
-*/
