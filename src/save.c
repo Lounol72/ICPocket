fab0cfb5a905bc99593ce46492ce7d20a32a36b6
lOkaal1 <littrewilliam@gmail.com>
2025-01-31 15:09:47 +0100

    Save final !

--- src/save.c
+++ src/save.c
@@ -13,8 +13,6 @@ void sauvegarder_Joueur(t_trainer * joueur,t_trainer * dresseur){
 
     joueur->id = 1;
     
-    printf("%s" , joueur->name);
-    getchar();
     FILE *fichier = fopen("src/data/save/saveJoueur.txt", "w+");
     if(fichier == NULL){
         printf("Erreur : impossible d'ouvrir le fichier.\n");
@@ -24,12 +22,23 @@ void sauvegarder_Joueur(t_trainer * joueur,t_trainer * dresseur){
     else{
         
         fprintf(fichier, "User name : %s\n", joueur->name);
-        fprintf(fichier, "Team : \n");
         for(int i = 0; i < 6; i++){
             for(int j = 0; j < 6; j++){
-                calcStatFrom(&(joueur->trainTeam->team[i]),joueur->trainTeam->team[i].baseStats[j]);
+                calcStatFrom(&(joueur->trainTeam->team[i]),j);
             }
-            fprintf(fichier, "Poke %d : %s\n", i+1, joueur->trainTeam->team[i].name);
+            fprintf(fichier, "%d\n", joueur->trainTeam->team[i].id);
+            fprintf(fichier, "%s\n", joueur->trainTeam->team[i].name);
+            fprintf(fichier, "%d\n", joueur->trainTeam->team[i].lvl);
+            fprintf(fichier, "%d\n", joueur->trainTeam->team[i].nature);
+            for(int j = 0 ; j < 6 ; j++)
+                fprintf(fichier, "%d\n", joueur->trainTeam->team[i].iv[j]);
+            fprintf(fichier, "%d\n", joueur->trainTeam->team[i].nb_move);
+            for(int j = 0 ; j < 1 ; j++)
+                fprintf(fichier, "%d\n", joueur->trainTeam->team[i].type[j]);
+            /*for(int j = 0 ; j < 1 ; j++)
+                fprintf(fichier, "%d\n", joueur->trainTeam->team[i].moveList[j]);*/ //Les moves ne sont pas encore init 
+
+            fprintf(fichier,"\n\n");
         }
 
         fprintf(fichier, "Dernier dresseur battu : %s id: %d\n", dresseur->name, dresseur->id);
@@ -38,16 +47,17 @@ void sauvegarder_Joueur(t_trainer * joueur,t_trainer * dresseur){
     
 }
 
+/*
 int main(){
     t_trainer  *red=malloc(sizeof(t_trainer));
     red->trainTeam = malloc(sizeof(t_Team));
     t_trainer *blue = malloc(sizeof(t_trainer));
     blue->id = 48;
     strcat(blue->name,"blue");
-
-    for(int i = 1; i < 7; i++){
+    for(int i = 0 ; i<6 ; i++){
         generate_poke(&(red->trainTeam->team[i]),"1");
     }
+    
     sauvegarder_Joueur(red,blue);
     for(int i = 0; i < 6; i++){
         
@@ -60,4 +70,4 @@ int main(){
     free(red);
     return 0;
 
-}
+}*/
