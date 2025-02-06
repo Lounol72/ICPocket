283e49e7ba0f53538e64c999604f129668added5
Natp24109 <nathanp2402@gmail.com>
2025-02-06 16:01:12 +0100

    started fixing trainer that could softlock game

--- src/trainerAI.c
+++ src/trainerAI.c
@@ -26,21 +26,24 @@ void insertionSort(int tabDegats[], int tabMove[], int n) {
 int AI_move_choice(t_AI * ai, t_Team * p){
 	int tab_damage[4]={-1};
 	int tab_move[4];
+    int nb_move_valide=0;
 	for(int i=0;i<ai->AI_t_Team->team[0].nb_move;i++){
-        tab_move[i]=i;
-		tab_damage[i]=calcDamage(ai->AI_t_Team,p,&(ai->AI_t_Team->team[0].moveList[i]));
+        tab_move[i]=ai->AI_t_Team->team[0].moveList[i].current_pp>0?i:-1;
+		tab_damage[i]=ai->AI_t_Team->team[0].moveList[i].current_pp>0?calcDamage(ai->AI_t_Team,p,&(ai->AI_t_Team->team[0].moveList[i])):-1;
+        nb_move_valide+=ai->AI_t_Team->team[0].moveList[i].current_pp>0?1:0;
 	}
+    if(nb_move_valide==0) return 0;
 	insertionSort(tab_damage,tab_move,ai->AI_t_Team->team[0].nb_move);
 
     int ind=0;
     if (ai->type%3==0){ // status_first AI
-        ind=(rand()%ai->AI_lvl)%ai->AI_t_Team->team[0].nb_move;
+        ind=(rand()%ai->AI_lvl)%nb_move_valide;
     }
     else if(ai->type%2==0){ // damage_first AI
-        while(ind<ai->AI_t_Team->team[0].nb_move && rand()%20>ai->AI_lvl) ind++;
+        while(ind<nb_move_valide-1 && rand()%20>ai->AI_lvl) ind++;
     }
     else{ //none type AI
-        ind=(rand()%ai->AI_lvl)%ai->AI_t_Team->team[0].nb_move;
+        ind=(rand()%ai->AI_lvl)%nb_move_valide;
     }
 	return tab_move[ind];
 }
